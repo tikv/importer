52bfb76c0775335db7ebdac128a13a08e910c98b
kennytm <kennytm@gmail.com>
2019-07-11 13:43:13 +0800

    Update TiKV dependency to 3.0.0.  (#8)
    * Cargo.toml: update tikv dependency to 3.0.0 GA
    
    Signed-off-by: kennytm <kennytm@gmail.com>
    
    * PD Connection: Retry for infinity, log periodically (#4502)
    
    Signed-off-by: Ana Hobden <operator@hoverbear.org>
    
    * engine: check iter status when scanning (#4863) (#4936)
    
    * engine: check iter status when scanning (#4863)
    
    Signed-off-by: Connor1996 <zbk602423539@gmail.com>
    
    * *: run rustfmt
    
    Signed-off-by: kennytm <kennytm@gmail.com>

--- tests/integrations/import/kv_service.rs
+++ tests/integrations/import/kv_service.rs
@@ -11,9 +11,9 @@ use grpcio::{ChannelBuilder, Environment, Result, WriteFlags};
 use kvproto::import_kvpb::*;
 use kvproto::import_kvpb_grpc::*;
 
+use test_util::retry;
 use tikv::config::TiKvConfig;
 use tikv_importer::import::ImportKVServer;
-use test_util::retry;
 
 fn new_kv_server() -> (ImportKVServer, ImportKvClient, TempDir) {
     let temp_dir = TempDir::new("test_import_kv_server").unwrap();
