9531815ad95fac8dc6febcf0fb8cd76d01662df3
kennytm <kennytm@gmail.com>
2020-06-22 16:41:39 +0800

    Cargo.toml: set version to 4.0.2 (#73)
    Signed-off-by: kennytm <kennytm@gmail.com>

--- src/import/status_server.rs
+++ src/import/status_server.rs
@@ -1,26 +1,47 @@
 // Copyright 2020 TiKV Project Authors. Licensed under Apache-2.0.
 
-use tikv::server::status_server::StatusServer as TiKVStatusServer;
-use tikv::config::ConfigController;
+use std::sync::Arc;
+
+use engine_rocks::RocksEngine;
+use raftstore::store::{transport::CasualRouter, CasualMessage};
 use security::SecurityConfig;
+use tikv::config::ConfigController;
+use tikv::server::status_server::StatusServer as TiKVStatusServer;
+
+#[derive(Clone)]
+struct MockRouter;
+
+impl CasualRouter<RocksEngine> for MockRouter {
+    fn send(&self, region_id: u64, _: CasualMessage<RocksEngine>) -> raftstore::Result<()> {
+        Err(raftstore::Error::RegionNotFound(region_id))
+    }
+}
 
 pub struct StatusServer {
-    inner_server: TiKVStatusServer,
+    inner_server: TiKVStatusServer<RocksEngine, MockRouter>,
     addr: String,
-    security_cfg: SecurityConfig,
 }
 
 impl StatusServer {
     pub fn new(addr: &str, security_cfg: SecurityConfig) -> StatusServer {
         StatusServer {
-            inner_server: TiKVStatusServer::new(1, None, ConfigController::default()),
+            inner_server: TiKVStatusServer::new(
+                1,
+                None,
+                ConfigController::default(),
+                Arc::new(security_cfg),
+                MockRouter,
+            )
+            .expect("failed to create status server"),
             addr: addr.to_owned(),
-            security_cfg,
         }
     }
 
     pub fn start(&mut self) {
-        if let Err(e) = self.inner_server.start(self.addr.clone(), &self.security_cfg) {
+        if let Err(e) = self
+            .inner_server
+            .start(self.addr.clone(), self.addr.clone())
+        {
             warn!("fail to setup status server: {:?}", e)
         }
     }
